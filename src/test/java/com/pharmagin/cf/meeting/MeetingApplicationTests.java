0fe0888eb2a502ee52eff479e978132804a7c03f
Ethan Zhang <zhjuncai@outlook.com>
2018-12-30 12:49:15 +0800

    Init empty spring boot project

---
+++ src/test/java/com/pharmagin/cf/meeting/MeetingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pharmagin.cf.meeting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeetingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
